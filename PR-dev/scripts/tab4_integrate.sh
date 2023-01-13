d9909aafd42aa75c2e0ab2a53c4dff1d216c9682
KasMasVan <51369075+KasMasVan@users.noreply.github.com>
2023-01-13 15:44:00 +0800

    Delete PR-dev directory

--- PR-dev/scripts/tab4_integrate.sh
+++
@@ -1,3 +0,0 @@
-python score_new.py obqa --model xl --prefix "Therefore, " --reweight 0.4;
-python score_new.py cqa --model xl --prefix "Deductively, " --reweight 1.3;
-python score_new.py copa --model xl --prefix "Consequently, " --reweight 2.0;
