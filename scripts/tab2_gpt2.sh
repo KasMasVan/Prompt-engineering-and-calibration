81dfe6f84c97d2586702ea46502540fb7da4edff
KasMasVan <51369075+KasMasVan@users.noreply.github.com>
2023-01-13 15:45:21 +0800

    Add files via upload

---
+++ scripts/tab2_gpt2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for model_size in "gpt2" "m" "l" "xl"
+do
+    python score_new.py obqa --model $model_size --reweight 0.4 
+    python score_new.py obqa --model $model_size --prefix "Therefore, " --reweight 0.4
+    python score_new.py cqa --model $model_size --reweight 1.3
+    python score_new.py cqa --model $model_size --prefix "Deductively, " --reweight 1.3
+    python score_new.py copa --model $model_size --reweight 2.0
+    python score_new.py copa --model $model_size --prefix "Consequently, " --reweight 2.0
+done
